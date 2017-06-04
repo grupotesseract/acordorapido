2cca42b8bce61b7063c21da1190c2a0ace4c1e54
vagrant <vagrant@homestead>
2017-06-04 20:55:30 +0000

    Alguns ajustes do layout

--- resources/views/vendor/adminlte/layouts/partials/sidebar.blade.php
+++ resources/views/vendor/adminlte/layouts/partials/sidebar.blade.php
@@ -33,13 +33,17 @@
         <ul class="sidebar-menu">
             <li class="header">{{ trans('adminlte_lang::message.header') }}</li>
             <!-- Optionally, you can add icons to the links -->
-            <li class="active"><a href="{{ url('home') }}"><i class='fa fa-link'></i> <span>{{ trans('adminlte_lang::message.home') }}</span></a></li>
-            <li><a href="#"><i class='fa fa-link'></i> <span>{{ trans('adminlte_lang::message.anotherlink') }}</span></a></li>
+            <li class="active"><a href="{{ url('home') }}"><i class='glyphicon glyphicon-home'></i> <span>{{ trans('adminlte_lang::message.home') }}</span></a></li>
+            <li><a href="#"><i class='glyphicon glyphicon-comment'></i> <span>Enviar SMS</span></a></li>
+            <li><a href="#"><i class='glyphicon glyphicon-stats'></i> <span>Relatórios</span></a></li>
             <li class="treeview">
-                <a href="#"><i class='fa fa-link'></i> <span>{{ trans('adminlte_lang::message.multilevel') }}</span> <i class="fa fa-angle-left pull-right"></i></a>
+                <a href="#"><i class='glyphicon glyphicon-import'></i> <span>Importações</span> <i class="fa fa-angle-left pull-right"></i></a>
                 <ul class="treeview-menu">
-                    <li><a href="#">{{ trans('adminlte_lang::message.linklevel2') }}</a></li>
-                    <li><a href="#">{{ trans('adminlte_lang::message.linklevel2') }}</a></li>
+                    <li><a href="#">Módulo Azul</a></li>
+                    <li><a href="#">Módulo Verde</a></li>
+                    <li><a href="#">Módulo Amarelo</a></li>
+                    <li><a href="#">Módulo Vermelho</a></li>
+                    
                 </ul>
             </li>
         </ul><!-- /.sidebar-menu -->
