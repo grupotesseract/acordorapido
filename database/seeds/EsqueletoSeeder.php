8e07e90f0168dfb2a50cba7a851152f0070d0a01
vagrant <vagrant@homestead>
2017-07-04 01:36:38 +0000

    Funcionando com as fitas do dododio

--- database/seeds/EsqueletoSeeder.php
+++ database/seeds/EsqueletoSeeder.php
@@ -1,7 +1,5 @@
 <?php
-
 use Illuminate\Database\Seeder;
-
 /**
  * Class EsqueletoSeeder
  */
@@ -16,102 +14,50 @@ class EsqueletoSeeder extends Seeder
     {
         try {
             
-
             // Criando usuário para aluno, escola e admin
             $userAluno = new App\User();
             $userAluno->name = "Evandro Carreira";
             $userAluno->email = "evandro.carreira@gmail.com";
             $userAluno->password = bcrypt(env('ADMIN_PWD', '123321'));
             $userAluno->save();
-
             $userEscola = new App\User();
             $userEscola->name = "ITE";
             $userEscola->email = "toledo@ite.edu.br";
             $userEscola->password = bcrypt(env('ADMIN_PWD', '123321'));
             $userEscola->save();
-
             $userAdmin = new App\User();
-            $userAdmin->name = "Edilson Britto";
+            $userAdmin->name = "Edilson Alexandre Britto";
             $userAdmin->email = "edilson.bauru@gmail.com";
             $userAdmin->password = bcrypt(env('ADMIN_PWD', '123321'));
             $userAdmin->save();
-
             // Criando entidades de aluno e escola e associando o user
             $c = new App\Cliente();
             $c->nome = "Evandro Barbosa Carreira";
             $c->rg = "46.755.061-2";
             $c->user()->associate($userAluno);
             $c->save();
-
             $e = new App\Empresa();
             $e->nome = "ITE";
             $e->cidade = "Bauru";
             $e->estado = "SP";
             $c->user()->associate($userEscola);
             $e->save();
-
-            $e = new App\Empresa();
-            $e->nome = "PREVE";
-            $e->cidade = "Bauru";
-            $e->estado = "SP";
-            $e->save();
-
-            $e = new App\Empresa();
-            $e->nome = "COEDUP";
-            $e->cidade = "Bauru";
-            $e->estado = "SP";
-            $e->save();
-
-
-            /*$u = new App\User();
-            $u->name = "Evandro Carreira";
-            $u->email = "evandro.carreira@gmail.com";
-            $u->password = bcrypt(env('ADMIN_PWD', '123321'));
-            $u->save();
-
-            
-
-            $c = new App\Cliente();
-            $c->nome = "Evandro Barbosa Carreira";
-            $c->rg = "46.755.061-2";
-            $c->user()->associate($u);
-=======
             // Criando funções dentro do sistema
             $escola = new App\Role();
             $escola->name         = 'escola';
             $escola->display_name = 'Instituição de ensino'; 
             $escola->description  = 'Usuário autorizado a visualizar os dados referentes à sua escola'; 
             $escola->save();
->>>>>>> 983e743feed8cfe86f76186c10e44ecca4072b10
-
             $admin = new App\Role();
             $admin->name         = 'admin';
             $admin->display_name = 'Admin do sistema';
             $admin->description  = 'Usuário autorizado a excluir, editar e incluir alunos, escolas e novos usuários'; 
             $admin->save();
-
             $aluno = new App\Role();
             $aluno->name         = 'aluno';
             $aluno->display_name = 'Usuário Aluno'; 
             $aluno->description  = 'Usuário autorizado a visualizar somente seus títulos e infos pessoais'; 
             $aluno->save();
-
-<<<<<<< HEAD
-            $t = new App\Titulo();
-            $t->estado = "verde";
-            $t->valor = 1380.45;
-            $t->titulo = "0834A34002017";
-            $t->pago = true;
-            $t->cliente()->associate($c);
-            $t->empresa()->associate($e);
-            $t->save();
-
-            $a = new App\Aviso();
-            $a->titulo = "Pague o aluguel";
-            $a->texto = "Olá! Para manter suas contas em dia, passe no banco na próxima semana";
-            $a->user()->associate($u);
-            $a->cliente()->associate($c);
-            $a->save();*/
             // associando as funções aos usuários
             $userAluno->attachRole($aluno);
             $userEscola->attachRole($escola);
